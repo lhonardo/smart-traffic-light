1d50e398d486b23f55ac8cc89b407d854353677e
Leonardo Santos <santos@devgrid.co.uk>
2019-06-17 22:23:07 -0300

    Add endpont to get light status

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const fetch = require('node-fetch');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get("/preference_light", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+  .get('/preference_light', (req, res, next) => {
+    fetch('https://smart-traffic-light-9acdb.firebaseio.com/preference_light.json')
+      .then(response => response.text())
+      .then((body) => {
+        res.send(body);
+      });
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
